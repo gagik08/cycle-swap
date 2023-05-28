95f6fd3c0fa1dba8c06a5e1a80eafc8ef7f40292
Gagik Kazandjyan <bi0820a@outlook.com>
2023-05-28 22:44:04 +0400

    Solved at 28.05.2023

--- src/main/java/com/epam/rd/autotasks/CycleSwap.java
+++ src/main/java/com/epam/rd/autotasks/CycleSwap.java
@@ -5,13 +5,10 @@ class CycleSwap {
         if (array.length <= 1) {
             return;
         }
-
         int lastElement = array[array.length - 1];
 
-        // Shift elements to the right
         System.arraycopy(array, 0, array, 1, array.length - 1);
 
-        // Set the first element to the original last element
         array[0] = lastElement;
     }
 
@@ -19,22 +16,18 @@ class CycleSwap {
         if (array.length <= 1) {
             return;
         }
-
-        shift %= array.length;  // Ensure the shift is within array length
+        shift %= array.length;
 
         if (shift == 0) {
-            return;  // No need to shift
+            return;
         }
 
         int[] temp = new int[shift];
 
-        // Copy the last 'shift' elements to the temporary array
         System.arraycopy(array, array.length - shift, temp, 0, shift);
 
-        // Shift elements to the right by 'shift' positions
         System.arraycopy(array, 0, array, shift, array.length - shift);
 
-        // Copy the elements from the temporary array to the beginning of the array
         System.arraycopy(temp, 0, array, 0, shift);
     }
 }
